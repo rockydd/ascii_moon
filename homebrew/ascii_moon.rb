559f15a8f237f9591f360c2da6d1a74f8b5ca52b
rocky <64197+rockydd@users.noreply.github.com>
2025-12-12 21:01:56 -0800

    chore(homebrew): update formula for v0.2.1

--- homebrew/ascii_moon.rb
+++ homebrew/ascii_moon.rb
@@ -1,8 +1,8 @@
 class AsciiMoon < Formula
   desc "A TUI to show the moon phase"
   homepage "https://github.com/rockydd/ascii_moon"
-  url "https://github.com/rockydd/ascii_moon/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7fcbed8d8012faf74995f912ee60ab97433ee3f2ad76dae858f437801e6c9604"
+  url "https://github.com/rockydd/ascii_moon/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "26dcfd49c1fceed69d25a973cdbb7fcb1fe520f20dd9d6dc8c9f10ee415e0ea7"
   license "MIT"
 
   depends_on "rust" => :build
